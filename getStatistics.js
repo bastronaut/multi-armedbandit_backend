1483877ed05ef58f22804643267bb5ddce25b3cb
bassijtsma <btsijtsma@gmail.com>
2015-12-06 15:34:42 +0100

    setup getting statistics

--- getStatistics.js
+++ getStatistics.js
@@ -1,1 +1,33 @@
 var connection = require('./dbconnection');
+
+
+
+function getStatisticsFromDB () {
+
+}
+
+
+function calculateAVGConversion () {
+
+}
+
+
+function selectColor () {
+
+}
+
+
+function returnStatistics () {
+
+}
+
+
+module.exports = {
+  getStatistics : function getStatistics(db, callback) {
+    getStatisticsFromDB;
+    calculateAVGConversion;
+    selectColor;
+    returnStatistics;
+  }
+
+}
